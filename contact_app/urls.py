94af2c0a84ab5f7255b03bee829d4b95cd61dc3b
mohdfaour03 <msf20@aub.edu.lb>
2024-11-29 17:37:34 +0200

    Model modification

--- contact_app/urls.py
+++ contact_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('', views.add_contact, name='add_contact'),  
+    path('', views.add_professional, name='add_contact'),  
     path('contacts/', views.contact_list, name='contact_list'),  
-    path('edit/<int:id>/', views.edit_contact, name='edit_contact'),  
-    path('delete/<int:id>/', views.delete_contact, name='delete_contact'), 
+    path('edit/<int:id>/', views.edit_professional, name='edit_contact'),  
+    path('delete/<int:id>/', views.delete_professional, name='delete_contact'), 
 ]
