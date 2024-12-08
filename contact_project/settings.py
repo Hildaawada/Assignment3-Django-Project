4c9597557b0ee7956360a6b3e7dc4edb1189822d
Hilda Awada <hildaawada10@gmail.com>
2024-12-08 23:09:46 +0200

    please work please pleaseeeeeeeee

--- contact_project/settings.py
+++ contact_project/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    
 ]
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # Add Whitenoise middleware
